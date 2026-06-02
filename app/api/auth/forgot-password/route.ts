c651a231a424de5a8ab030396513be04c8622e5a
lithiago <thiagoramon993@gmail.com>
2026-06-02 12:16:00 -0300

    fix email

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ ok: true })
   } catch (error) {
-    console.error(error)
+    console.log(error)
     return NextResponse.json({ message: 'Internal server error' }, { status: 500 })
   }
 }
